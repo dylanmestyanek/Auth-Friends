6bd9e7cccf120df51ce73591778fec9946c6ccb6
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-10-15 15:23:49 -0500

    Friends list is now rendering the friends, only when a token exists in localStorage

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,8 +1,24 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+
+import authWithAxios from "../utils/authWithAxios"
+import Friend from "./Friend"
 
 const FriendsList = () => {
+    const [friends, setFriends] = useState([])
+
+    useEffect(() => {
+        authWithAxios().get("/friends")
+            .then(res => setFriends(res.data))
+            .catch(err => console.log(err.response))
+    }, [])
+
     return (
-        <h1>Friends List!</h1>
+        <>
+            <h1>Friends List!</h1>
+            {
+                friends.map((friend, idx) => <Friend key={idx} friend={friend} />)
+            }
+        </>
     )
 }
 
